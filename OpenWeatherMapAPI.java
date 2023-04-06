788aa3dcad6a34bcbd7212ef26ffda78c62dbcea
Arya Maddel <84805906+aryamaddel@users.noreply.github.com>
2023-04-06 23:24:35 +0530

    API Client 🌤️

---
+++ OpenWeatherMapAPI.java
@@ -0,0 +1,88 @@
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Scanner;
+
+import org.json.JSONObject;
+
+public class OpenWeatherMapAPI{
+    private final String API_KEY;
+    private final String API_URL;
+    private final JSONObject weatherData;
+
+    /**
+     * Constructs an API object for the specified city.
+     *
+     * @param city the city name
+     * @throws IOException if an I/O error occurs
+     */
+    public OpenWeatherMapAPI(String city) throws IOException{
+        API_KEY = "YOUR_API_KEY"; // Get your API key from https://openweathermap.org/api
+        API_URL = "https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid="+API_KEY+"&units=metric";
+        
+        URL weatherUrl = new URL(API_URL);
+        HttpURLConnection connection = (HttpURLConnection) weatherUrl.openConnection();
+        connection.setRequestMethod("GET");
+
+        if (connection.getResponseCode() != HttpURLConnection.HTTP_OK) {
+            throw new RuntimeException("Failed : HTTP error code : "
+                    + connection.getResponseCode());
+        }
+
+        try (Scanner urlScanner = new Scanner(connection.getInputStream())) {
+            weatherData = new JSONObject(urlScanner.nextLine());
+        }
+    }
+
+
+    public double getTemperature(){
+        return weatherData.getJSONObject("main").getDouble("temp");
+    }
+
+
+    public double getMinTemperature(){
+        return weatherData.getJSONObject("main").getDouble("temp_min");
+    }
+
+
+    public double getMaxTemperature(){
+        return weatherData.getJSONObject("main").getDouble("temp_max");
+    }
+
+
+    public String getWeatherCondition(){
+        return weatherData.getJSONArray("weather").getJSONObject(0).getString("main");
+    }
+
+
+    public double getHumidity(){
+        return weatherData.getJSONObject("main").getDouble("humidity");
+    }
+
+
+    public double getWindSpeed(){
+        return weatherData.getJSONObject("wind").getDouble("speed");
+    }
+
+
+    public double getWindDirection(){
+        return weatherData.getJSONObject("wind").getDouble("deg");
+    }
+
+
+    public long getSunriseTime(){
+        return weatherData.getJSONObject("sys").getLong("sunrise");
+    }
+
+
+    public long getSunsetTime(){
+        return weatherData.getJSONObject("sys").getLong("sunset");
+    }
+
+
+    public double getVisibility(){
+        return weatherData.getDouble("visibility");
+    }
+}
+
+
